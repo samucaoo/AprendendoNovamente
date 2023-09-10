ecd94cef35a2d339f13033c28b706d9a9b4a836b
Viniseeus <vinigolarte@gmail.com>
2023-09-09 21:53:37 -0300

    Adicionei uma nova linha

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ class Churrasco:
         print(f"O dia do evento será {self.dia}")
         print(f"A quantidade de carne para {pessoas} pessoas é de {totalcarne} Gramas")
         print("Teste do commit no github")
+        print("Aqui foi o vini que fez")
 
 
